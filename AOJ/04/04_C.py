11d33b154f8628a575f766ffb4784e2f635bcd39
ryo <bluestone2853@gmail.com>
2022-09-17 02:41:16 +0900

    fix AOJ4

--- AOJ/04/04_C.py
+++ AOJ/04/04_C.py
@@ -1,14 +1,29 @@
-a, op, b = input().split()
+def plus(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
+
+def div(a, b):
+    return int(a / b)
+
+S = open(0)
+a, op, b = S.readline().split()
+a, b = map(int, [a, b])
 
 while op != '?':
     if op == '+':
-        print(int(a) + int(b))
+        print(plus(a, b))
     elif op == '-':
-        print(int(a) - int(b))
+        print(minus(a, b))
     elif op == '*':
-        print(int(a) * int(b))
+        print(multi(a, b))
     elif op == '/':
-        print(int(int(a) / int(b)))
+        print(div(a, b))
 
-    a, op, b = input().split()
+    a, op, b = S.readline().split()
+    a, b = map(int, [a, b])
 
